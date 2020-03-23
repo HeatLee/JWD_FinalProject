eb8d441c92149922af159130c873e16e98fdccf0
Sasha <unheatlee@gmail.com>
2020-03-23 15:29:54 +0300

    Add address class

---
+++ src/main/java/by/training/finalproject/entity/Address.java
@@ -0,0 +1,51 @@
+package by.training.finalproject.entity;
+
+import java.util.Objects;
+
+public class Address {
+    private String country;
+    private String town;
+
+    public Address(String country, String town) {
+        this.country = country;
+        this.town = town;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(country, address.country) &&
+                Objects.equals(town, address.town);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, town);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "country='" + country + '\'' +
+                ", town='" + town + '\'' +
+                '}';
+    }
+}
