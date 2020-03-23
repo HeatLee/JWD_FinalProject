1f50f6cd6ad0b37695ad0c75f99ba07d3d0cbe0d
Sasha <unheatlee@gmail.com>
2020-03-23 15:21:52 +0300

    Add reservation user class

---
+++ src/main/java/by/training/finalproject/entity/User.java
@@ -0,0 +1,79 @@
+package by.training.finalproject.entity;
+
+import java.util.Objects;
+
+public class User {
+    private int userId;
+    private String login;
+    private String password;
+    private String email;
+    private UserRole userRole;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(email, user.email) &&
+                userRole == user.userRole;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, login, password, email, userRole);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", userRole=" + userRole +
+                '}';
+    }
+}
