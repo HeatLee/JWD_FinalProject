b8c7dfa7b106bacef2b67346fd47ea5b1a108acc
Sasha Markevich <unheatlee@gmail.com>
2020-04-09 14:36:42 +0300

    Delete AbstractBuilder class

--- src/main/java/by/training/finalproject/builder/AbstractBuilder.java
+++
@@ -1,9 +0,0 @@
-package by.training.finalproject.builder;
-
-public abstract class AbstractBuilder<T>{
-    protected T businessEntity;
-
-    public T build() {
-        return businessEntity;
-    }
-}
