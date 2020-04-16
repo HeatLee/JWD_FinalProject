b0d96de7594ccc2200214e420a5c21c51dbba203
Sasha Markevich <unheatlee@gmail.com>
2020-04-16 17:37:40 +0300

    Improove service interface structure

--- src/main/java/by/training/finalproject/service/UserService.java
+++ src/main/java/by/training/finalproject/service/UserService.java
@@ -4,8 +4,7 @@ import by.training.finalproject.entity.User;
 import by.training.finalproject.exception.ServiceException;
 
 public interface UserService {
+    User signIn(User user) throws ServiceException;
 
-    User getUserByLogin(String login) throws ServiceException;
-
-    User getUserByEmail(String email) throws ServiceException;
+    User signUp(User user) throws ServiceException;
 }
