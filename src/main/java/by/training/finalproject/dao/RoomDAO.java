c293470e8cfdfa48c6cb1007601cd852365dd784
Sasha Markevich <unheatlee@gmail.com>
2020-05-14 08:51:34 +0300

    Improve DAO arcitecture

---
+++ src/main/java/by/training/finalproject/dao/RoomDAO.java
@@ -0,0 +1,13 @@
+package by.training.finalproject.dao;
+
+import by.training.finalproject.entity.Room;
+import by.training.finalproject.entity.RoomStatus;
+import by.training.finalproject.exception.DAOException;
+
+import java.util.List;
+
+public interface RoomDAO<T> extends CommonDAO<T> {
+    List<Room> readByRoomStatus(RoomStatus roomStatus) throws DAOException;
+
+    List<Room> readByCapacity(int capacity) throws DAOException;
+}
