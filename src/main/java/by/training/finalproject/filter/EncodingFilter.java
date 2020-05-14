558c6b952ce6c087df79dc43dd96ed253a0c60b3
Sasha Markevich <unheatlee@gmail.com>
2020-05-14 08:53:44 +0300

    Add encoding filter

---
+++ src/main/java/by/training/finalproject/filter/EncodingFilter.java
@@ -0,0 +1,17 @@
+package by.training.finalproject.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"})
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain next)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        next.doFilter(request, response);
+    }
+}
