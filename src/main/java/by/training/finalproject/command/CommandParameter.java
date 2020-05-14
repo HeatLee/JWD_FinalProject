9ab9f14c990ca36bfcc717ca19ce2d10f1618f26
Sasha Markevich <unheatlee@gmail.com>
2020-05-14 09:35:15 +0300

    Major improvements

--- src/main/java/by/training/finalproject/command/CommandParameter.java
+++ src/main/java/by/training/finalproject/command/CommandParameter.java
@@ -1,21 +1,20 @@
 package by.training.finalproject.command;
 
 import by.training.finalproject.command.impl.*;
-import by.training.finalproject.command.redirect.IndexRedirectCommand;
-import by.training.finalproject.command.redirect.ProfileRedirectCommand;
-import by.training.finalproject.command.redirect.SignInRedirectCommand;
-import by.training.finalproject.command.redirect.SingUpRedirectCommand;
+import by.training.finalproject.command.redirect.*;
 
 public enum CommandParameter {
     SIGN_UP(new SingUpCommand()),
     SIGN_IN(new SignInCommand()),
-    SIGN_OUT(new SignOutCommand()),
-    CHANGE_USER(new ChangeUserInfoCommand()),
-    INDEX_PAGE(new IndexRedirectCommand()),
     SIGN_UP_PAGE(new SingUpRedirectCommand()),
     SIGN_IN_PAGE(new SignInRedirectCommand()),
+    INDEX_PAGE(new IndexRedirectCommand()),
+
+    SIGN_OUT(new SignOutCommand()),
+    CHANGE_USER(new ChangeUserInfoCommand()),
     PROFILE_PAGE(new ProfileRedirectCommand()),
-    SEND_REQUEST(new AddNewRequestCommand());
+    SEND_REQUEST(new AddNewRequestCommand()),
+
     ADMIN_PAGE(new AdminPageRedirectCommand());
 
     private Command command;
